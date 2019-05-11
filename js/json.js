2bd362b486bbe8b4dce38c08f28c23dafb23b864
浮生未歇 <lch1349509294@gmail.com>
2019-05-11 19:54:37 +0800

    update json

--- js/json.js
+++ js/json.js
@@ -1,6 +1,5 @@
 const jsonData = [{
-	"search": [
-		{
+	"search": [{
 			"name": "谷歌",
 			"hostname": "www.google.com",
 			"value": "https://www.google.com/search?q=%s&num=20&newwindow=1"
@@ -12,150 +11,144 @@ const jsonData = [{
 			"name": "京东",
 			"hostname": "www.jd.com",
 			"value": "https://search.jd.com/Search?keyword=%s&enc=utf-8"
+		}, {
+			"name": "知乎",
+			"hostname": "www.zhihu.com",
+			"value": "https://www.zhihu.com/search?type=content&q=%s"
 		}
 	],
-	"card": [
-		{
-			"title": "常用网站",
-			"content": [
-				{
-					"name": "Google应用商店",
-					"url": "https://chrome.google.com/webstore/category/extensions?hl=zh-CN"
-				}, {
-					"name": "YouTube",
-					"url": "https://www.youtube.com"
-				}, {
-					"name": "哔哩哔哩",
-					"url": "https://www.bilibili.com"
-				}, {
-					"name": "百度翻译",
-					"url": "http://fanyi.baidu.com/"
-				}, {
-					"name": "谷歌翻译",
-					"url": "https://translate.google.cn/"
-				}, {
-					"name": "在线打字测试",
-					"url": "http://dazi.kukuw.com/index.html"
-				}, {
-					"name": "Greasy Fork",
-					"url": "https://greasyfork.org/zh-CN"
-				}, {
-					"name": "Userstyles",
-					"url": "https://userstyles.org/"
-				}, {
-					"name": "Github",
-					"url": "https://github.com/"
-				}, {
-					"name": "和彩云",
-					"url": "http://caiyun.feixin.10086.cn/portal/index.jsp#myfile"
-				}, {
-					"name": "Mozilla开发者中心",
-					"url": "https://developer.mozilla.org/zh-CN/"
-				}, {
-					"name": "open-open",
-					"url": "http://www.open-open.com/lib/tag/PHP"
-				}
-			]
+	"card": [{
+		"title": "常用网站",
+		"content": [{
+			"name": "Google应用商店",
+			"url": "https://chrome.google.com/webstore/category/extensions?hl=zh-CN"
 		}, {
-			"title": "常用工具",
-			"content": [
-				{
-					"name": "IP",
-					"url": "https://ip.cn/"
-				}, {
-					"name": "壁纸",
-					"url": "http://wallls.com/"
-				}, {
-					"name": "转换工具",
-					"url": "https://www.bejson.com/"
-				}, {
-					"name": "网速测试",
-					"url": "http://www.speedtest.net/"
-				}, {
-					"name": "正则实测",
-					"url": "http://www.atool.org/regex.php"
-				}, {
-					"name": "图片压缩",
-					"url": "https://tinypng.com/"
-				}
-			]
+			"name": "YouTube",
+			"url": "https://www.youtube.com"
 		}, {
-			"title": "Linux",
-			"content": [
-				{
-					"name": "Linxu命令查询",
-					"url": "http://linux.51yip.com/"
-				}, {
-					"name": "Linux命令大全",
-					"url": "http://man.linuxde.net/"
-				}, {
-					"name": "Debian手册",
-					"url": "https://www.debian.org/doc/manuals/debian-reference/index.zh-cn.html"
-				}
-			]
+			"name": "哔哩哔哩",
+			"url": "https://www.bilibili.com"
 		}, {
-			"title": "开发手册",
-			"content": [
-				{
-					"name": "BootStrap",
-					"url": "http://www.bootcss.com/"
-				}, {
-					"name": "Cheatsheet",
-					"url": "https://hackerthemes.com/bootstrap-cheatsheet/"
-				} , {
-					"name": "jQuery",
-					"url": "http://jquery.cuishifeng.cn/"
-				} , {
-					"name": "Vue",
-					"url": "https://cn.vuejs.org/v2/guide/"
-				} , {
-					"name": "PHP",
-					"url": "http://php.net/"
-				}, {
-					"name": "CSS3",
-					"url": "http://www.css88.com/book/css/"
-				}
-			]
+			"name": "百度翻译",
+			"url": "http://fanyi.baidu.com/"
 		}, {
-			"title": "CDN",
-			"content": [
-				{
-					"name": "BootCDN",
-					"url": "http://www.bootcdn.cn/"
-				}, {
-					"name": "360",
-					"url": "https://cdn.baomitu.com/"
-				}, {
-					"name": "七牛",
-					"url": "https://www.staticfile.org/"
-				},{
-					"name":"Jsdelivr",
-					"url":"https://www.jsdelivr.com/"
-				},{
-					"name":"BeeCDN",
-					"url":"https://www.beecdn.com/"
-				}
-			]
+			"name": "谷歌翻译",
+			"url": "https://translate.google.cn/"
 		}, {
-			"title": "软件",
-			"content": [
-				{
-					"name": "奇客小栈",
-					"url": "http://www.geekotg.com/"
-				} , {
-					"name": "安下网",
-					"url": "http://anxia.com/"
-				} , {
-					"name": "绿色资源网",
-					"url": "http://www.downcc.com/"
-				}, {
-					"name": "绿色软件",
-					"url": "https://www.nocmd.com/"
-				},{
-					"name":"殁漂遥",
-					"url":"https://www.laomoit.com/"
-				}
-			]
+			"name": "在线打字测试",
+			"url": "http://dazi.kukuw.com/index.html"
+		}, {
+			"name": "Greasy Fork",
+			"url": "https://greasyfork.org/zh-CN"
+		}, {
+			"name": "Userstyles",
+			"url": "https://userstyles.org/"
+		}, {
+			"name": "Github",
+			"url": "https://github.com/"
+		}, {
+			"name": "和彩云",
+			"url": "http://caiyun.feixin.10086.cn/portal/index.jsp#myfile"
+		}, {
+			"name": "Mozilla开发者中心",
+			"url": "https://developer.mozilla.org/zh-CN/"
+		}, {
+			"name": "open-open",
+			"url": "http://www.open-open.com/lib/tag/PHP"
+		}, {
+			"name": "BT资源",
+			"url": "https://lai.yuweining.cn/archives/909/"
 		}
-	]
+		]
+	}, {
+		"title": "常用工具",
+		"content": [{
+			"name": "IP",
+			"url": "https://ip.cn/"
+		}, {
+			"name": "壁纸",
+			"url": "http://desk.zol.com.cn/"
+		}, {
+			"name": "转换工具",
+			"url": "https://www.bejson.com/"
+		}, {
+			"name": "网速测试",
+			"url": "http://www.speedtest.net/"
+		}, {
+			"name": "正则实测",
+			"url": "http://www.atool.org/regex.php"
+		}, {
+			"name": "图片压缩",
+			"url": "https://tinypng.com/"
+		}]
+	}, {
+		"title": "Linux",
+		"content": [{
+			"name": "Linxu命令查询",
+			"url": "http://linux.51yip.com/"
+		}, {
+			"name": "Linux命令大全",
+			"url": "http://man.linuxde.net/"
+		}, {
+			"name": "Debian手册",
+			"url": "https://www.debian.org/doc/manuals/debian-reference/index.zh-cn.html"
+		}]
+	}, {
+		"title": "开发手册",
+		"content": [{
+			"name": "BootStrap",
+			"url": "http://www.bootcss.com/"
+		}, {
+			"name": "Cheatsheet",
+			"url": "https://hackerthemes.com/bootstrap-cheatsheet/"
+		}, {
+			"name": "jQuery",
+			"url": "http://jquery.cuishifeng.cn/"
+		}, {
+			"name": "Vue",
+			"url": "https://cn.vuejs.org/v2/guide/"
+		}, {
+			"name": "PHP",
+			"url": "http://php.net/"
+		}, {
+			"name": "CSS3",
+			"url": "http://www.css88.com/book/css/"
+		}]
+	}, {
+		"title": "CDN",
+		"content": [{
+			"name": "BootCDN",
+			"url": "http://www.bootcdn.cn/"
+		}, {
+			"name": "360",
+			"url": "https://cdn.baomitu.com/"
+		}, {
+			"name": "七牛",
+			"url": "https://www.staticfile.org/"
+		}, {
+			"name": "Jsdelivr",
+			"url": "https://www.jsdelivr.com/"
+		}, {
+			"name": "BeeCDN",
+			"url": "https://www.beecdn.com/"
+		}]
+	}, {
+		"title": "软件",
+		"content": [{
+			"name": "奇客小栈",
+			"url": "http://www.geekotg.com/"
+		}, {
+			"name": "安下网",
+			"url": "http://anxia.com/"
+		}, {
+			"name": "绿色资源网",
+			"url": "http://www.downcc.com/"
+		}, {
+			"name": "绿色软件",
+			"url": "https://www.nocmd.com/"
+		}, {
+			"name": "殁漂遥",
+			"url": "https://www.laomoit.com/"
+		}]
+	}]
 }][0];
